d38f6d4594900fd77d1ebba29b3da8b639bba2dc
Sourabh <vermasourabh512@gmail.com>
2023-10-23 14:35:40 +0530

    changes done

--- controller/userDetails.js
+++ controller/userDetails.js
@@ -3,6 +3,9 @@ const myUserSchema=require('../schema/formSchema');
 exports.addUser=async(req,res)=>{
     console.log("user started adding");
     const {name,age,phoneNumber,gender}=req.body;
+    console.log("myuserSchema",myUserSchema);
+    const user=await myUserSchema.find({name});
+    if(user){return res.status(404).json({error:"error"});};
     const data=await myUserSchema.create({
         name,age,phoneNumber,gender
     });

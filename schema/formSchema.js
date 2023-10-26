029800f4470a3619ab58d098c3227e9f13bb3bfe
Sourabh <vermasourabh512@gmail.com>
2023-10-26 21:49:00 +0530

    done

--- schema/formSchema.js
+++ schema/formSchema.js
@@ -2,9 +2,9 @@ const mongoose=require('mongoose');
 
 const userSchema=mongoose.Schema({
     name:{type:String,unique:true},
-    age:{type:String,unique:true},
+    age:{type:String},
     phoneNumber:{type:Number,unique:true},
-    gender:{type:String,unique:true}
+    gender:{type:String}
 });
 
 module.exports=mongoose.model("myUserSchema",userSchema);

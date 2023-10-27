f3aa467e354b4b8215341cabdd01432c335f0d62
Sourabh <vermasourabh512@gmail.com>
2023-10-27 11:26:15 +0530

    done

--- controller/userDetails.js
+++ controller/userDetails.js
@@ -4,8 +4,10 @@ exports.addUser=async(req,res)=>{
     console.log("user started adding");
     const {name,age,phoneNumber,gender}=req.body;
     console.log("myuserSchema",myUserSchema);
-    const user=await myUserSchema.find({name});
-    if(user){return res.status(404).json({error:"error"});};
+    const user=await myUserSchema.findOne({name});
+    console.log("ArpitLoveVatsala");
+    if(user){return res.status(404).json({error:"new error"});}
+    console.log("user",user);
     const data=await myUserSchema.create({
         name,age,phoneNumber,gender
     });
